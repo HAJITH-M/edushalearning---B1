838b1de19ccb261125284779a4951883456b814f
HAJITH-M <hajithm2003@gmail.com>
2024-06-11 20:16:56 +0530

    Day - 11 code added

---
+++ Day-11 js/mapMethod.js
@@ -0,0 +1,22 @@
+arr1 = [1,2,"name", true];
+
+// for(let a=0; a<10; a++){
+//     console.log(a);
+// }
+
+// console.log(arr1);
+// arr1.forEach((element)=>{
+//     console.log(element);
+// })
+
+// arr2 = [7,8,9, "ram", "vikram", true]
+
+// arr2.map((element)=>{
+//     console.log(element);
+// })
+
+arr3 = [1,2,3,4,5,6,7]
+arr3.filter((element) => {
+
+    console.log(element > 5)
+})
